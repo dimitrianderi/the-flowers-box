91ffe246dbf30bce99e82c8eca253c12ea1d68f8
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-10 21:38:23 +0500

    feat: add styles for navbar

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="menu">
-    <div class="menu__logo">
-      <img class="menu__logo-img" src="@@/img/logo.png" alt="logo">
-    </div>
-    <nav class="menu__navbar">
-      <ul class="nav">
-        <li class="nav__item"><a href="#" class="nav__link">Link1</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Ссылка2</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Ссылка3</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Ссылка4</a></li>
-      </ul>
-      <div class="icons">
-        <font-awesome-icon class="icon" icon="fa-solid fa-right-to-bracket" title="login"/>
-        <font-awesome-icon class="icon" icon="fa-solid fa-address-card" title="registration"/>
+    <div class="container">
+      <div class="menu__logo">
+        <img class="menu__logo-img" src="@@/img/logo.png" alt="logo" />
       </div>
-    </nav>
+      <nav class="menu__navbar">
+        <ul class="nav">
+          <li class="nav__item"><a href="#" class="nav__link">Link1</a></li>
+          <li class="nav__item"><a href="#" class="nav__link">Ссылка2</a></li>
+          <li class="nav__item"><a href="#" class="nav__link">Ссылка3</a></li>
+          <li class="nav__item"><a href="#" class="nav__link">Ссылка4</a></li>
+        </ul>
+        <div class="icons">
+          <font-awesome-icon
+            class="icon"
+            icon="fa-solid fa-user"
+            title="log in"
+          />
+          <font-awesome-icon
+            class="icon"
+            icon="fa-solid fa-address-card"
+            title="sign up"
+          />
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAddressCard, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard, faRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons'
 import router from '@/router'
 import App from '@/App.vue'
 import './assets/styles/index.scss'
 
-library.add(faRightToBracket, faAddressCard)
+library.add(faRightToBracket, faAddressCard, faUser)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
