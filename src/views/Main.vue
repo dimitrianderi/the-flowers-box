7f0da8ed7e1ddb46081f5a2a2da5fc6b6fd68eaa
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-18 21:13:40 +0500

    feat: add builder

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,6 +3,7 @@
   <main>
     <the-achievements></the-achievements>
     <the-portfolio></the-portfolio>
+    <the-builder></the-builder>
   </main>
   <the-footer></the-footer>
 </template>
@@ -12,8 +13,9 @@ import TheHeader from '@/components/TheHeader.vue'
 import TheFooter from '@/components/TheFooter.vue'
 import TheAchievements from '@/components/TheAchievements.vue'
 import ThePortfolio from '@/components/ThePortfolio.vue'
+import TheBuilder from '@/components/TheBuilder.vue'
 export default {
-  components: { TheHeader, TheFooter, TheAchievements, ThePortfolio },
+  components: { TheHeader, TheFooter, TheAchievements, ThePortfolio, TheBuilder },
   name: 'Main',
 }
 </script>
