9a3cb11fdecd545cd8b769541507e6ab19ca6db7
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-19 20:31:18 +0500

    feat: add adaptive for buiding

---
+++ src/config/the-builder.js
@@ -0,0 +1,9 @@
+export const builderData = {
+    title: 'Bouquet Builder',
+
+    description: 'create your own flower bouquet using our exclusive bouquet builder!',
+
+    text: 'Create unique, personalised bouquets online and make your flower bouquet extra special. Make your own bouquet choosing your favourite flowers and you’re guaranteed to be satisfied. Buying a bouquet for a loved one? Pick flowers that you know they’ll love and you’ll definitely make them smile!',
+    
+    btn: 'build your bouquet!'
+}
