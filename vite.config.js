0b4e13d58d42f7accc6a379b3deacd2267f955bb
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-29 19:16:52 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '//',
+  base: '/flowers-box/',
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
