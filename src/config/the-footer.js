f5ab97c763a0b0b0f37ff2d41deefa90a9b27698
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-31 00:15:28 +0500

    feat: add eslint

--- src/config/the-footer.js
+++ src/config/the-footer.js
@@ -30,4 +30,4 @@ export const icons = [
     },
 ]
 
-export const mapUrl = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2997.515803860258!2d69.27738272485348!3d41.29764133476705!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b3fbd7ff15f%3A0xe194e52b898e3ded!2z0KHRgtGD0LTQuNGPINGE0LvQvtGA0LjRgdGC0LjQutC4IEZsYXdiZXJyeQ!5e0!3m2!1sru!2sus!4v1703950026417!5m2!1sru!2sus";
+export const mapUrl = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2997.515803860258!2d69.27738272485348!3d41.29764133476705!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b3fbd7ff15f%3A0xe194e52b898e3ded!2z0KHRgtGD0LTQuNGPINGE0LvQvtGA0LjRgdGC0LjQutC4IEZsYXdiZXJyeQ!5e0!3m2!1sru!2sus!4v1703950026417!5m2!1sru!2sus"
