4977232a7f5d08b1648abee0bffdf26b26c360b3
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-12-31 21:37:15 +0500

    fix: add to vite.config base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/the-flowers-box/',
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: [
