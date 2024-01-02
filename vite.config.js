bdf8c364deca1800d3331bca4dc1fdff85c4ee70
Dmitry Kharitonov <dimitrianderi@gmail.com>
2024-01-02 18:31:37 +0500

    fix: correct vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import * as path from 'path';
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/the-flowers-box/',
   plugins: [vue(), eslintPlugin()],
